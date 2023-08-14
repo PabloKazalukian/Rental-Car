79ce1e4b498d92a21180436c170b8f421ad790ea
pablo <kazalukianpablo@gmail.com>
2023-08-14 16:40:12 -0300

    check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,35 +6,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarsComponent } from './components/cars/cars.component';
 import { StoreModule } from '@ngrx/store';
-import { carReducer} from './components/cars/cars.reducers'
+import { carReducer } from './components/cars/cars.reducers';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ShowCarComponent } from './components/cars/show-car/show-car.component';
 import { FilterCarsComponent } from './components/cars/filter-cars/filter-cars.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { NavBarComponent} from './components/navbar/navbar.component';
+import { NavBarComponent } from './components/navbar/navbar.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { FormularioComponent } from './components/contact/formulario/formulario.component';
 import { HomeComponent } from './components/home/home.component';
 import { Error404Component } from './components/error404/error404.component';
-import { HttpClientModule} from '@angular/common/http'
-import { EffectsModule} from '@ngrx/effects';
-import {carEffects} from './components/cars/car.effects';
+import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
+import { carEffects } from './components/cars/car.effects';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule} from '@angular/material/toolbar';
-import { MatButtonModule} from '@angular/material/button';
-import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { TokenInterceptor } from './services/interceptor/token.interceptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { LoadingComponent } from './components/loading/loading.component';
 import { CarApplicationComponent } from './components/car-application/car-application.component';
 import { DialogLoggedComponent } from './components/cars/show-car/dialog-logged/dialog-logged.component';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { LandingPageComponent } from './components/home/landing-page/landing-page.component';
 import { HowToUseComponent } from './components/home/how-to-use/how-to-use.component';
-import {MatCheckboxModule} from '@angular/material/checkbox'
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -55,13 +55,12 @@ import {MatCheckboxModule} from '@angular/material/checkbox'
     DialogLoggedComponent,
     LandingPageComponent,
     HowToUseComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    StoreModule.forRoot({autos: carReducer}),
+    StoreModule.forRoot({ autos: carReducer }),
     StoreDevtoolsModule.instrument({
       maxAge: 5, // Retains last 5 states
       logOnly: environment.production, // Restrict extension to log-only mode
@@ -76,8 +75,7 @@ import {MatCheckboxModule} from '@angular/material/checkbox'
     MatSidenavModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatCheckboxModule
-
+    MatCheckboxModule,
   ],
   providers: [
     {
@@ -86,6 +84,6 @@ import {MatCheckboxModule} from '@angular/material/checkbox'
       multi: true,
     },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,83 +1,81 @@
 import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {  BehaviorSubject, map, Observable, throwError } from 'rxjs';
+import { BehaviorSubject, map, Observable, throwError } from 'rxjs';
 import { Car } from '../core/models/car.interface';
 import { catchError } from 'rxjs/operators';
-import {JwtHelperService} from '@auth0/angular-jwt'
-import { Login,LoginResponde } from '../core/models/login.interface';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Login, LoginResponde } from '../core/models/login.interface';
 import { usuario } from '../core/models/user.interface';
 
-const helper= new JwtHelperService;
-
-
+const helper = new JwtHelperService();
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-  private loggetIn= new BehaviorSubject<boolean>(false)
-  private user = new BehaviorSubject<usuario>({username:'',userId:0})
+  private loggetIn = new BehaviorSubject<boolean>(false);
+  private user = new BehaviorSubject<usuario>({ username: '', userId: 0 });
 
-  private readonly API= `${environment.api}/auth/login`;
-  token?:string;
+  private readonly API = `${environment.api}/auth/login`;
+  token?: string;
 
-  constructor(private readonly http:HttpClient) {
+  constructor(private readonly http: HttpClient) {
     this.checkToken();
   }
 
-  checkLogin(form:Login):Observable<boolean | void>{
-    return this.http.post<LoginResponde>(this.API,form)
-    .pipe(
-      map( (res:LoginResponde)=>{
-          this.saveToken(res.token);
-          this.loggetIn.next(true);
-          if(res) return true
-          else return false
-        }
-      )
+  checkLogin(form: Login): Observable<boolean | void> {
+    return this.http.post<LoginResponde>(this.API, form).pipe(
+      map((res: LoginResponde) => {
+        this.saveToken(res.token);
+        this.loggetIn.next(true);
+        if (res) return true;
+        else return false;
+      })
     );
   }
 
-  isLoggin():Observable<boolean>{
+  isLoggin(): Observable<boolean> {
     return this.loggetIn.asObservable();
   }
 
-  logout():void{
-    localStorage.removeItem('auth')
+  logout(): void {
+    localStorage.removeItem('auth');
     this.loggetIn.next(false);
   }
 
-  private checkToken():void{
-
+  private checkToken(): void {
     const userToken = localStorage.getItem('auth');
-    if(userToken !== null){
+    if (userToken !== null) {
       const isExpired = helper.isTokenExpired(userToken);
-      !isExpired ? this.loggetIn.next(true) : this.logout()
-
+      !isExpired ? this.loggetIn.next(true) : this.logout();
     }
     //set userIsLogged = isExpired
-
   }
 
-  readToken ():Observable<usuario> {
+  readToken(): Observable<usuario> {
     const userToken = localStorage.getItem('auth');
-    if(userToken){
-      this.user.next(helper.decodeToken(userToken))
+    console.log(userToken);
+    if (userToken) {
+      const user = helper.decodeToken(userToken);
+      this.user.next(user);
     }
-    return this.user.asObservable()
+    return this.user.asObservable();
   }
 
-  private saveToken (token:string):void{
-    localStorage.setItem('auth',token);
+  private saveToken(token: string): void {
+    localStorage.setItem('auth', token);
     const userToken = localStorage.getItem('auth');
-    if(userToken !== null){
-      this.user.next(helper.decodeToken(userToken))
+    // console.log(JwtHelperService);
+    if (userToken !== null) {
+      const user = helper.decodeToken(userToken);
+
+      this.user.next(user);
     }
     this.loggetIn.next(true);
   }
 
-  getToken():string | null{
+  getToken(): string | null {
     const userToken = localStorage.getItem('auth');
 
     return userToken;
