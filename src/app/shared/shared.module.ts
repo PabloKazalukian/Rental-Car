e934660ba2201df5d5ed678eb7956003ac9da5eb
pablo <kazalukianpablo@gmail.com>
2025-07-07 20:41:10 -0300

    css boostrap to BEM page:cars sahred user

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ErrorComponent } from 'src/app/shared/components/ui/error/error.component';
 import { SuccessComponent } from 'src/app/shared/components/ui/success/success.component';
 import { MessageErrorComponent } from 'src/app/shared/components/ui/message-error/message-error.component';
 import { LoadingComponent } from 'src/app/shared/components/ui/loading/loading.component';
@@ -14,7 +13,6 @@ import { MaterialModule } from './material.module';
 @NgModule({
     declarations: [
         SuccessComponent,
-        ErrorComponent,
         MessageErrorComponent,
         LoadingComponent,
         StepperComponent,
@@ -28,7 +26,6 @@ import { MaterialModule } from './material.module';
     ],
     exports: [
         SuccessComponent,
-        ErrorComponent,
         MessageErrorComponent,
         LoadingComponent,
         StepperComponent,
