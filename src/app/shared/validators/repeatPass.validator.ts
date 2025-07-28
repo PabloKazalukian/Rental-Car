aff503a544f724256496d41858b4e6996f02baed
pablo <kazalukianpablo@gmail.com>
2025-07-28 19:01:02 -0300

    register

--- src/app/shared/validators/repeatPass.validator.ts
+++ src/app/shared/validators/repeatPass.validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidationErrors } from "@angular/forms"
 
 export class repeatPass {
     static samePassword(control: AbstractControl): ValidationErrors | null {
-        if ((control.value.password1.length >= 3) && (control.value.password2.length >= 3) && (control.value.password1 !== control.value.password2)) {
+        if ((control.value.password1?.length >= 3) && (control.value.password2?.length >= 3) && (control.value.password1 !== control.value.password2)) {
             return { passNoRepeat: true }
         } else return null
     }
