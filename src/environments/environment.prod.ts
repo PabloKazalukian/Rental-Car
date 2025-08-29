d6cffefb69f824809e14fb9a805f5b05a2a0cd1f
Pablo Kaza <kazalukianpablo@gmail.com>
2025-08-29 17:38:34 -0300

    fix socket enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     // api: 'https://web-production-6900.up.railway.app/api'
     api: 'https://api-rental-car-9niy.onrender.com/api',
-    googleClientId: '921923361549-msk10ulmksa5uqkrjhne7n4roj7a7hkc.apps.googleusercontent.com'
-
+    socket: 'https://api-rental-car-9niy.onrender.com',
+    googleClientId: '921923361549-msk10ulmksa5uqkrjhne7n4roj7a7hkc.apps.googleusercontent.com',
 };
