f4e4f2be57b9aa40546549515414c8289a94e09d
Pablo Kaza <kazalukianpablo@gmail.com>
2026-02-24 13:42:51 -0300

    auht

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-    HttpRequest,
-    HttpHandler,
-    HttpEvent,
-    HttpInterceptor
-} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
 import { LoginService } from '../services/auth/login.service'
